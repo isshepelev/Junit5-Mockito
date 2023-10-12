3af32e7f44546ec2a45007de6203937143753b8a
Ilya <isshepelev2004@gmail.com>
2023-10-12 22:37:11 +0300

    add crud test

--- src/test/java/ru/isshepelev/mockmvc/controller/BookControllerTest.java
+++ src/test/java/ru/isshepelev/mockmvc/controller/BookControllerTest.java
@@ -58,7 +58,7 @@ class BookControllerTest {
                                 [
                                     {
                                         "id": 1,
-                                        "name": "sperma",
+                                        "name": "name",
                                         "author": "Ilya"
                                     },
                                     {
